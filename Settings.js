0ecd5f9d28fc065df97d3c996b88cb063c438d25
DerGaijin <44649120+DerGaijin@users.noreply.github.com>
2025-04-24 19:15:51 +0200

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -64,40 +64,6 @@ var LinkGroups = [
 		Label: "Privat",
 		IsEncrypted: true,
 		IsPrivate: true,
-		Links: [
-			{
-				Label: "U2FsdGVkX18KIKj2RA+kSB/OxuMQG1NvHyN5cYMqLOk=",
-				URL: "U2FsdGVkX1+88amK18vu6DLAthIqMIZ1ojZzlDxxLf6CPrJ0emSAoWayzR7+y0Yj",
-			},
-			{
-				Label: "U2FsdGVkX18y4wIwOMipdpaJ+u2PVrX7sB6aChbwoeg=",
-				URL: "U2FsdGVkX1+jFYHySYDZnAiX7eoIpDElyb5kp/POFk656B9BAgRZEzhXDi3NHg0r",
-			},
-			{
-				Label: "U2FsdGVkX1+oj2yLLqK+e2VrK99K0Bqyg72tXXmoveQ=",
-				URL: "U2FsdGVkX1/7fH4pOpuk4DpXAyEKS8Ztp4xltsXroZUrmrbjyC0C1ZzpQqbw3JwK",
-			},
-			{
-				Label: "U2FsdGVkX1+tr1d0jeD408T6haX+sAxMiPsuyFDqX94=",
-				URL: "U2FsdGVkX18+72Kq5guQEiZ6MAbVbba3iVyN3Ts6uNQx7i9sR8WyUaCYXrfi4gdN",
-			},
-			{
-				Label: "U2FsdGVkX1+AiG3juCgpW9E2+Y4LktAxh8Wp7ZDYVGU=",
-				URL: "U2FsdGVkX1+vv8kEsmuoLHKnCdvoCikeetfycMfCPMTticMwQh6eVPf6R5jVpUOm",
-			},
-			{
-				Label: "U2FsdGVkX1+p0X1MqlrQeDLWc6yO0GZC+C/LaGmaZgU=",
-				URL: "U2FsdGVkX1+KsgZSfNgNj+0yMZVTj7Al5NAxPQ4B2cIrpcf0CCqNdb5/vTObKJKqnEEvxWomQfuAT+HEsVjV7RBZZpePs6sgeRxl/Kr9Vkork8Hwps4hrTvvEeYzuZm4",
-				Icon: "U2FsdGVkX190FsFRcmvvAZh/o352ipBezgkSGdSIbWQUrhD90w4Qq/08j3vQ/3dOZ2xdEk/8oayZUj5owdSERciKqxVWvf0fNRZmsjGn1NI=",
-			},
-			{
-				Label: "U2FsdGVkX19bgVlxzA8P61K1O17fWEg8HZRs7E6fU00=",
-				URL: "U2FsdGVkX1/XWku6dndmFxMoXf9IJ6DOAW8VZjet4Nzhhh0JSK1rrqJtRVCyX8ma38QF5vfERlqBoCzNQ4c0MA==",
-			},
-			{
-				Label: "U2FsdGVkX1+6ThnloUkWk5Hvc77Df2U2uU5A/Nh8V18=",
-				URL: "U2FsdGVkX1+XAopQqerNG9WXMWQlLa1L+LtTVC3bipa/4M6h1ipB8GYAjmlRfr96",
-			}
-		],
+		Links: [],
 	},
 ];
